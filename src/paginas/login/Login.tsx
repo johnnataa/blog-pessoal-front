7da24cef282a2c4abc264825b9615b4dc82ac041
johnnataa <johnnata_@outlook.com>
2024-02-18 12:39:00 -0300

    criação da Home

---
+++ src/paginas/login/Login.tsx
@@ -0,0 +1,21 @@
+import { useNavigate, Link } from 'react-router-dom'
+
+const Login = () => {
+    const navigate = useNavigate()
+    return (
+        <div>
+            <h2 className="text-slate-900 text-5xl   m-4">Login</h2>
+            <div>
+                <button type='submit'
+                    onClick={() => { navigate('/home') }}
+                    className='hover:underline mx-4'>
+                    Home useNavigate
+                </button>
+                <Link to='/home' className='hover:underline  mx-4'>Home por Link</Link>
+            </div>
+
+        </div>
+    )
+}
+
+export default Login;
