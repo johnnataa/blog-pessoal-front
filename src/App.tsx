6ac28daea3133686f9282bbc11079a3a9d57e9ef
johnnataa <johnnata_@outlook.com>
2024-02-19 16:42:36 -0300

    Finalizando Home e Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import './App.css';
-import Home from './paginas/home/Home';
+
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cadastro from './paginas/cadastro/Cadastro';
 import Login from './paginas/login/Login';
+import Home from './paginas/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className='min-h-[80vh]'>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+   <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+        </AuthProvider>   
     </>
-);
+  );
 }
 export default App;
-
