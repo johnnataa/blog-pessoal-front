85489700aff2dadcef8d151c41729a953bf91e8f
johnnataa <johnnata_@outlook.com>
2024-02-22 23:39:14 -0300

    Criação do CRUD de Postagens

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,20 +1,22 @@
-import homeLogo from '../../assets/img/casa.png'
+import homeLogo from '../../assets/img/home.png'
+import ListaPostagens from '../../postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../postagens/modalPostagem/ModalPostagem';
 import './Home.css';
 
 
 function Home() {
-    return (
-        <>
-        <div className="bg-indigo-900 flex justify-center">
-          <div className='container grid grid-cols-2 text-white'>
-            <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
-  
-              <div className="flex justify-around gap-4">
-              
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-              </div>
+  return (
+      <>
+      <div className="bg-indigo-900 flex justify-center">
+        <div className='container grid grid-cols-2 text-white'>
+          <div className="flex flex-col gap-4 items-center justify-center py-4">
+            <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
+            <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+
+            <div className="flex justify-around gap-4">
+            <ModalPostagem />
+              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+            </div>
             </div>
   
             <div className="flex justify-center ">
@@ -23,7 +25,7 @@ function Home() {
             </div>
           </div>
         </div>
-      
+        <ListaPostagens />
       </>
     );
 }
